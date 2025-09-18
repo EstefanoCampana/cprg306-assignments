6e7c84cef21df46e2afb939b3878e981ad29d521
EstefanoCampana <Estefano.CampanaMunoz@edu.sait.ca>
2025-09-18 12:34:02 -0600

    Styling main page

--- app/page.js
+++ app/page.js
@@ -3,16 +3,20 @@ import Link from "next/link";
 export default function Home(){
   return (
     <main>
-      <div className="p-3 m-3">
-      <h1 className="text-4xl">CPRG306 ASSIGNMENTS</h1>  
+      <div className="p-3 m-3 flex justify-center">
+      <h1 className="text-4xl text-pink-700 font-bold">CPRG306 ASSIGNMENTS</h1>  
       </div>
       <div className="p-3 m-3">
       <ul>
         <li>
-          <Link className="text-teal-400 hover:text-amber-50 font-bold text-3xl" href="./week-2">Week 2 Assignment</Link>
+          <button className="p-3 m-3 bg-gradient-to-r from-pink-300 to-red-200 rounded-lg hover:scale-105 transition-transform">
+            <Link className="text-pink-600 hover:text-amber-50 font-bold text-3xl" href="./week-2">Week 2 Assignment</Link>
+          </button>
         </li>
         <li>
-          <Link className="text-teal-400 hover:text-amber-50 font-bold text-3xl" href="./week-3">Week 3 Assignment</Link>
+          <button className="p-3 m-3 bg-gradient-to-r from-purple-300 to-blue-200 rounded-lg hover:scale-105 transition-transform">
+            <Link className="text-purple-600 hover:text-amber-50 font-bold text-3xl" href="./week-3">Week 3 Assignment</Link>
+          </button>
         </li>
       </ul>
       </div>
